93f06596efed34d91d3f2f0b6c1f1539b18d9ad0
sancousik <znosok2012@gmail.com>
2024-03-11 08:18:49 +0300

    Add validation to searh param

--- AGSR.Patients/AGSR.Patients/Validators/SearchByDateRequestValidator.cs
+++ AGSR.Patients/AGSR.Patients/Validators/SearchByDateRequestValidator.cs
@@ -1,8 +1,16 @@
 ﻿using AGSR.Patients.Requests;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace AGSR.Patients.Validators;
 
 public class SearchByDateRequestValidator : AbstractValidator<SearchByDateRequest>
 {
+    private const string regex = "^(^(?i)[A-Z]{2})?([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\\.[0-9]{1,9})?)?)?(Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?$";
+
+    public SearchByDateRequestValidator()
+    {
+        RuleFor(x => x.date)
+            .ForEach(x => x.Matches(regex, RegexOptions.Compiled));
+    }
 }
